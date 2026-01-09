616bc8cc810d91788b76c4f1f17d4f015bf1c3a9
waqarahmad29042004 <waqarahmad2904@gmail.com>
2026-01-09 18:51:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def get_prediction():
     baby_df = pd.DataFrame(baby_data_cleaned)
 
     #load machine learning trained model
-    with open("model/model.pkl",'rb') as obj:
+    with open("model.pkl",'rb') as obj:
         model = pickle.load(obj)
 
     #make prediction on user data
